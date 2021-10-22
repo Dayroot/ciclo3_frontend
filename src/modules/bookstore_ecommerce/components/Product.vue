1aa977ade4bd261ff30a7a9ea5569ca6090d4d33
Dayroot <dallancaceresp@gmail.com>
2021-10-22 11:14:13 -0500

    organizacion del componente de producto y la pagina para ver los detalles del producto

---
+++ src/modules/bookstore_ecommerce/components/Product.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="product">
+        <p>{{title}}</p>
+    </div>
+    <router-link :to="{name: 'product-book'}"></router-link>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            productData : {
+                image: 'https://imgur.com/j3Am7gI',
+                title: "Libro de prueba",
+                tipo: "Libro",
+                author: "autor",
+                price: "56000"
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/ColorPalette.scss";
+
+.product {
+    width: 300px;
+    height: 400px;
+    background: $yellow-solid;
+}
+    
+</style>
---
+++ src/modules/bookstore_ecommerce/pages/DetailProductPage.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="detail-container">
+        <p>{{description}}</p>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            productData : {
+                image: 'https://imgur.com/j3Am7gI',
+                title: "Libro de prueba",
+                tipo: "Libro",
+                author: "autor",
+                price: "56000",
+                description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos debitis deleniti, accusantium, voluptates qui sequi cupiditate eveniet, facere quidem minus pariatur veritatis sit autem omnis modi quam quaerat sed natus Consequatur eius nisi excepturi ratione consectetur ipsa nemo nihil dolorem labore blanditiis, placeat distinctio doloremque. Veritatis nulla natus necessitatibus magni deleniti quisquam autem. Eaque harum nemo cupiditate rerum totam? Praesentium."
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .container {
+        height: 400px;
+        width: 500px;
+        background: cadetblue;
+    }
+</style>
