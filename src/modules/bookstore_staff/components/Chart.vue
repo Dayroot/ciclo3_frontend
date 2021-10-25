d3d371a57e0055d9320a7caec76129bae16e2930
anagarcia791 <amgt2697@gmail.com>
2021-10-25 16:48:10 -0500

    side bar analysis

--- src/modules/bookstore_staff/components/Chart.vue
+++ src/modules/bookstore_staff/components/Chart.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="chart">
-    
+    <p>GRAFICO</p>
     </div >
 </template>
 
--- src/modules/bookstore_staff/pages/AnalysisPage.vue
+++ src/modules/bookstore_staff/pages/AnalysisPage.vue
@@ -19,25 +19,25 @@
                         product: 1,
                         quantity: 8,
                         type: "magazine",
-                        price: 64750
+                        price: 64750,
                         total: 518000, 
-                        date: '2020-3-3'
-                    }
+                        date: '2020-3-3',
+                    },
                     {
                         product: 3,
                         quantity: 13,
                         type: "magazine",
                         price: 43522, 
                         total: 522264,
-                        date: '2020-3-3'
-                    }
+                        date: '2020-3-3',
+                    },
                     {
                         product: 6,
                         quantity: 5,
                         type: "book",
                         price: 55238, 
                         total: 276190,
-                        date: '2020-3-3'
+                        date: '2020-3-3',
                     }
                 ],
             }
